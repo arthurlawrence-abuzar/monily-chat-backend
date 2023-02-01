04b34679a697e8e7e8e7dead3aee70cc71416939
arthurlawrence-abuzar <99425289+arthurlawrence-abuzar@users.noreply.github.com>
2023-02-02 00:43:49 +0530

    added changes

---
+++ src/chatServer.js
@@ -0,0 +1,33 @@
+// const path = require('path');
+const express = require("express");
+const app = express();
+const http = require("http");
+const serverless = require("serverless-http");
+const server = http.createServer(app);
+const socketio = require("socket.io");
+const io = socketio(server, {
+  cors: {
+    origin: "*",
+  },
+});
+io.on("connection", (socket) => {
+  console.log("a user connected :D");
+  socket.on("message", (message) => {
+    const obj = JSON.stringify(message);
+    io.emit("message", `${obj}`);
+    socket.broadcast.to(message.recieverId).emit("message", `${obj}`);
+    console.log(message);
+  });
+  socket.on("delete", (message) => {
+    const obj = JSON.stringify(message);
+    io.emit("delete", `${obj}`);
+    socket.broadcast.to(message.recieverId).emit("delete", `${obj}`);
+    console.log(message);
+  });
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
